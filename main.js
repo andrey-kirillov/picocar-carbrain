d0c18bb3b4f519ec4328114f0a75d31b43bbe638
andrey-kirillov <andrey.kirillov.kiev@gmail.com>
2024-03-13 00:41:28 +0200

    emit to dist, one file

--- main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var builtInLed_1 = require("./modules/builtInLed");
-// LED
-var led = new builtInLed_1.default();
-led.toggleWithInterval();
-// Buzzer
-// const buzzer = new Buzzer();
-// buzzer.playTest();
--- main.ts
+++ main.ts
@@ -1,10 +1,31 @@
-import BuiltInLed from './modules/builtInLed';
-import Buzzer from './modules/buzzer';
+// import BuiltInLed from './modules/builtInLed';
+// import Buzzer from './modules/buzzer';
+const {Servo} = require('servo');
 
-// LED
-const led = new BuiltInLed();
-led.toggleWithInterval();
+/** Servo **/
+const servo = new Servo();
+// attach to GP15
+servo.attach(7);
+// Turn from 0 to 180 by 10 degrees
+// for (let i = 0; i <= 180; i = i+10) {
+//     console.log(i);
+//     servo.write(i);
+//     delay(500);
+// }
+servo.write(0);
+delay(2500);
+servo.write(90);
+delay(2500);
+servo.write(180);
+delay(2500);
+servo.write(90);
+delay(2500);
+servo.write(0);
 
-// Buzzer
+/** LED **/
+// const led = new BuiltInLed();
+// led.toggleWithInterval();
+
+/** Buzzer **/
 // const buzzer = new Buzzer();
 // buzzer.playTest();
